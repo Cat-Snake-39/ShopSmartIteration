78e9c9a4f2c69160c73a09c587dbc7717e313458
Janis-H <janis_h.aguilar@yahoo.com>
2020-11-05 10:59:24 -0800

    (hotfix) removed bug where multiple databse entries would be added for the same user if they logged in again

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,13 +12,26 @@ userController.create = async (req, res, next) => {
       Authorization: `token ${res.locals.user.token}`,
     },
   }).then(({ data: { login } }) => {
-    // insert into users table 
-    const insertQuery = `INSERT INTO users (username, token) VALUES ('${login}', '${res.locals.user.token}');`
-    db.query(insertQuery, (err, data) => {
-      if (err) return next({ err, log: 'Error inserting user into database in userController.create' });
-      console.log('user created!');
+    // first check if user exists in database
+    const userQuery = `SELECT username FROM users WHERE username = '${login}';`;
+    db.query(userQuery, (err, userQueryData) => {
+      // if an error occurs, invoke global error handler
+      if (err) next({ err, log: 'Error querying for existing user in userController.create' });
+      // if rowCount is equal to 0, meaning user doesn't exist
+      if (userQueryData.rowCount === 0) {
+        // insert into users table 
+        const insertQuery = `INSERT INTO users (username, token) VALUES ('${login}', '${res.locals.user.token}');`
+        db.query(insertQuery, (err, data) => {
+          // if there's an error trying to insert, invoke global error handler
+          if (err) return next({ err, log: 'Error inserting user into database in userController.create' });
+          console.log('user created!');
+          // move onto next middleware
+          return next();
+        });
+      }
+      // else move onto next middleware
       return next();
-    });
+    })
   })
     .catch(err => console.log(err));
 };
